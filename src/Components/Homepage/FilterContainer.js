0e14fe97d882577ba88ce73eafd6a2a5bfc81a2f
5steellix8 <metehankasapp@gmail.com>
2021-09-11 21:33:57 +0300

    main

--- src/Components/Homepage/FilterContainer.js
+++ src/Components/Homepage/FilterContainer.js
@@ -45,39 +45,118 @@ const useStyles = makeStyles((theme) => ({
   packagesContainer: {
     display: "flex",
     justifyContent: "space-between",
+    padding: "3rem 0",
   },
   packagesCardInner: {
     borderRadius: "0px",
+    position: "relative",
+    overflow: "visible",
+  },
+  packagesAbsoluteTop: {
+    position: "absolute",
+    top: "-10px",
+    right: "-10px",
+    display: "flex",
+    flexDirection: "row",
+  },
+  packagesAbsSinirsiz: {
+    backgroundColor: "rgb(235, 151, 0)",
+    marginLeft: "10px",
+    color: "white",
+    fontFamily: "VodafoneFont, sans-serif",
+    fontSize: "12px",
+    padding: "0 2px",
+  },
+  packagesAbsOnlineOzel: {
+    backgroundColor: "red",
+    color: "white",
+    padding: "0 2px",
+    fontFamily: "VodafoneFont, sans-serif",
+    fontSize: "12px",
   },
   PackageFirstAreaContainer: {
     background: "rgb(56,39,39)",
     background:
       "linear-gradient(90deg, rgba(56,39,39,1) 0%, rgba(170,23,0,1) 100%)",
   },
-  packageName:{
-    color:"white",
-    fontSize:"16px",
-    fontWeight:"300",
-    fontFamily:"VodafoneFont, sans-serif",
-    backgroundColor:"rgba(0,0,0,.2)",
+  packageName: {
+    color: "white",
+    fontSize: "16px",
+    fontWeight: "300",
+    fontFamily: "VodafoneFont, sans-serif",
+    backgroundColor: "rgba(0,0,0,.2)",
     padding: "6px",
   },
-  fsArea:{
-      padding:"15px",
-      paddingBottom:"30px",
-      color:"#fff",
+  fsArea: {
+    padding: "15px",
+    paddingBottom: "30px",
+    color: "#fff",
+    position:"relative"
   },
-  basicSubtitle:{
-    fontSize:"13px",
-    fontWeight:"100",
-    
+  fsAbs:{
+    position:"absolute",
+    bottom:"-10px",
+    left:"50%",
+    transform:"translateX(-50%)",
+    backgroundColor:"rgb(89, 13, 211)",
+    width:"65%",
+    borderRadius:"25px",
+    fontFamily: "VodafoneFont, sans-serif",
+    fontSize:"13px"
   },
-  packagePrice:{
-    fontSize:"45px",
-    lineHeight:"12px",
-    margin:"2rem 0",
-    marginRight:"5px"
-  }
+  basicSubtitle: {
+    fontSize: "13px",
+    fontWeight: "100",
+  },
+  packagePrice: {
+    fontSize: "43px",
+    lineHeight: "12px",
+    margin: "2rem 0",
+    marginRight: "5px",
+    fontFamily: "VodafoneFont, sans-serif",
+  },
+  ftContainer: {
+    padding: "20px",
+  },
+  packageDetail: {
+    fontSize: "30px",
+    fontWeight: "500",
+    fontFamily: "VodafoneFont, sans-serif",
+  },
+  greyColor: {
+    color: "#888",
+    marginLeft: "5px",
+    fontSize: "30px",
+    fontWeight: "500",
+    fontFamily: "VodafoneFont, sans-serif",
+  },
+  packageDescription: {
+    fontSize: "16px",
+    fontFamily: "VodafoneFont, sans-serif",
+    borderBottom: "1px solid rgba(0,0,0,.1)",
+    padding: "5px",
+    fontWeight: "700",
+    color: "black",
+  },
+  packageDetailSecond: {
+    fontSize: "15px",
+    fontFamily: "VodafoneFont, sans-serif",
+    borderBottom: "1px solid rgba(0,0,0,.1)",
+    padding: "5px",
+    fontWeight: "100",
+  },
+  detayButton: {
+    borderRadius: "20px",
+    border: "1px solid red",
+    color: "rgba(0,0,0,.8)",
+    margin: "15px",
+    fontFamily: "VodafoneFont, sans-serif",
+    fontSize: "13px",
+    padding: "2px 25px",
+    "&:hover": {
+      backgroundColor: "#fff",
+    },
+  },
 }));
 
 const Tags = [
@@ -152,12 +231,37 @@ const FilterContainer = () => {
       </Grid>
       <Grid container className={classes.packagesContainer}>
         {PackagesList.filter(
-          (packageName) => packageName.priceThreeMonth > 55
+          (packageName) => packageName.priceThreeMonth > 54
         ).map((filteredPackage) => (
           <Grid key={filteredPackage.key} className={classes.packagesCard}>
             <Card className={classes.packagesCardInner}>
+              <Box className={classes.packagesAbsoluteTop}>
+                {filteredPackage.OnlineOzel ? (
+                  <Typography
+                    variant="subtitle1"
+                    align="center"
+                    className={classes.packagesAbsOnlineOzel}
+                  >
+                    Online'a Özel İndirimli
+                  </Typography>
+                ) : (
+                  null
+                )}
+                {filteredPackage.Sinirsiz ? (
+                  <Typography
+                  variant="subtitle1"
+                  align="center"
+                  className={classes.packagesAbsSinirsiz}
+                >
+                  Sinirsiz
+                </Typography>
+                ) : (
+                  null
+                )}
+                
+              </Box>
               <Box className={classes.PackageFirstAreaContainer}>
-                <Box >
+                <Box>
                   <Typography
                     variant="subtitle1"
                     align="center"
@@ -167,6 +271,17 @@ const FilterContainer = () => {
                   </Typography>
                 </Box>
                 <Box className={classes.fsArea}>
+                {filteredPackage.specialOffer ? (
+                  <Typography
+                  variant="subtitle1"
+                  align="center"
+                  className={classes.fsAbs}
+                >
+                  {filteredPackage.specialOffer}
+                </Typography>
+                ) : (
+                  null
+                )}
                   <Typography
                     variant="subtitle1"
                     align="center"
@@ -180,7 +295,7 @@ const FilterContainer = () => {
                       display: "flex",
                       flexDirection: "row",
                       justifyContent: "center",
-                      alignItems:"end"
+                      alignItems: "end",
                     }}
                   >
                     <Typography
@@ -207,14 +322,20 @@ const FilterContainer = () => {
                   </Typography>
                 </Box>
               </Box>
-              <Box>
-                <Typography
-                  variant="subtitle1"
-                  align="center"
-                  className={classes.packageDetail}
-                >
-                  {filteredPackage.packageDetail}
-                </Typography>
+              <Box className={classes.ftContainer}>
+                <Box sx={{ display: "flex", justifyContent: "center" }}>
+                  <Typography
+                    variant="subtitle1"
+                    align="center"
+                    className={classes.packageDetail}
+                  >
+                    {filteredPackage.packageDetail}
+                  </Typography>
+                  <Typography variant="subtitle1" className={classes.greyColor}>
+                    {" "}
+                    + SINIRSIZ
+                  </Typography>
+                </Box>
                 <Typography
                   variant="subtitle1"
                   align="center"
@@ -231,7 +352,9 @@ const FilterContainer = () => {
                 </Typography>
 
                 <Box fullWidth sx={{ textAlign: "center" }}>
-                  <Button>Detayları Göster</Button>
+                  <Button className={classes.detayButton}>
+                    Detayları Göster
+                  </Button>
                 </Box>
               </Box>
             </Card>
@@ -251,8 +374,9 @@ const PackagesList = [
     OnlineOzel: true,
     Sinirsiz: true,
     priceThreeMonth: 55,
+    specialOffer:"Sınırsız TikTok paketi 25TL yerine 15TL!",
     priceNineMonth: 64,
-    packageDetail: "20 GB + SINIRSIZ",
+    packageDetail: "20 GB",
     packageFree: ["Whatsapp", "Messenger"],
     packageDescription: "ilk ay 20 GB hediye!",
     packageDetailSecond: "1000 DK + 250 SMS",
@@ -269,10 +393,31 @@ const PackagesList = [
     key: 2,
     packageName: "Red 20 GB",
     OnlineOzel: true,
+    Sinirsiz: false,
+    priceThreeMonth: 55,
+    priceNineMonth: 64,
+    packageDetail: "20 GB",
+    packageFree: ["Whatsapp", "Messenger"],
+    packageDescription: "ilk ay 20 GB hediye!",
+    packageDetailSecond: "1000 DK + 250 SMS",
+    showDetails: [
+      "Vodafonelular ve sabit hatlarla sınırsız konuşabilirsiniz.",
+      "Yeni gelen Redlilere özel Sınırsız Tiktok paketi aylık 25TL yerine 15TL! Bu paket ile Tiktok uygulaması yönüne internet kullanımınız GB kotanızdan düşmez.",
+      "Vodafone TV Red Paketi aylık 2GB interneti ile 12 ay boyunca hediye!",
+      "Ücretsiz araç dezenfeksiyonundan Trendyol'da kargo indirimine kadar bir çok ayrıcalık Vodafone Yanımda'da!",
+      "Taahhütsüz Aylık Tarife Ücreti: 94TL",
+      "İlk ay geçerli 20 GB hediyen Vodafone Yanımda'da seni bekliyor!",
+    ],
+  },
+  {
+    key: 3,
+    packageName: "Red 20 GB",
+    OnlineOzel: false,
     Sinirsiz: true,
     priceThreeMonth: 100,
+    specialOffer:"Sınırsız TikTok paketi 25TL yerine 15TL!",
     priceNineMonth: 64,
-    packageDetail: "20 GB + SINIRSIZ",
+    packageDetail: "20 GB",
     packageFree: ["whatsapp", "messenger"],
     packageDescription: "ilk ay 20 GB hediye!",
     packageDetailSecond: "1000 DK + 250 SMS",
